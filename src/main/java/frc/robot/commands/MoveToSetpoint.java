f4ff03a9b7227f8ced5f1b7a370e2bb8271dd967
Dhyan Soni <dhyantsoni@email.com>
2024-12-19 00:05:40 -0800

    setup command for setpoint

---
+++ src/main/java/frc/robot/commands/MoveToSetpoint.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Robot;
+
+public class MoveToSetpoint extends Command {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+    private double setpoint;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public MoveToSetpoint(double setpoint) {
+        this.setpoint = setpoint;
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        Robot.arm.moveToSetpoint(setpoint);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
